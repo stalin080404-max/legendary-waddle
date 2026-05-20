0f761b13ad5ab126816b56aa5a6de31ce50111b4
stalin jalagam <stalin0804@gmail.com>
2026-05-20 14:14:53 +0200

    Initial commit

--- hello.py
+++ hello.py
@@ -1,3 +1,8 @@
 def prod_integers(num1,num2):
     print("hello")
     return num1 * num2
+def add_integers(num1,num2):
+    print("hello")
+    return num1 + num2
+prod_integers(1,1)
+add_integers(1,1)
