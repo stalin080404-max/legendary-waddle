f58175b66ecf12b8f5c1796a9eca1e3b19d3cec9
stalin jalagam <stalin0804@gmail.com>
2026-05-26 13:49:52 +0200

    Updated file permissions

--- double_it.py
+++ double_it.py
@@ -12,3 +12,6 @@ def double_int(x: int | float | str |list[int | float | str]) -> (int | float | 
 if __name__ == "__main__":
     print(double_int(2))
     print("Final line in this module - The end!")
+    print(double_int(2))
+    print("Final line in this module - The end!")
+    
